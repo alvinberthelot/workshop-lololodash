8343d98184e01bea0314ec1c46d2b2e0b5134cba
Alvin BERTHELOT <alvin.berthelot@webyousoon.com>
2017-02-08 17:57:14 +0100

    (lololodash/9) : Start templating Done

--- program.js
+++ program.js
@@ -3,34 +3,7 @@ var _ = require("lodash");
 
 var worker = function(input) {
 
-
-  var underperform = [];
-  var overperform = [];
-
-  var average = _.reduce(input, (sum, freelancer, index, collection) => {
-    return sum + freelancer.income;
-  }, 0) / input.length;
-
-  function betterThanAverage(average, freelance) {
-    return freelance.income > average;
-  }
-
-  _.forEach(input, freelance => {
-    if (betterThanAverage(average, freelance)) {
-      overperform.push(freelance);
-    } else {
-      underperform.push(freelance);
-    }
-  })
-
-  var underperformSorted = _.sortBy(underperform, 'income');
-  var overperformSorted = _.sortBy(overperform, 'income');
-
-  return {
-    average: average,
-    underperform: underperformSorted,
-    overperform: overperformSorted
-  };
+  return _.template(`Hello ${input.name} (logins: ${input.login.length})`)(input);
 };
 
 // export the worker function as a nodejs module
