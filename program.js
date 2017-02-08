3f5b12b94833c580deaa7f74a7fc05f3bae82a97
Alvin BERTHELOT <alvin.berthelot@webyousoon.com>
2017-02-08 17:49:56 +0100

    (lololodash/8) : Analyze Done

--- program.js
+++ program.js
@@ -3,17 +3,34 @@ var _ = require("lodash");
 
 var worker = function(input) {
 
-  return _.chain(input)
-    .groupBy('article')
-    .map((command, key) => {
-      return {
-        article: +key,
-        total_orders: _.reduce(command, (sum, item) => sum + item.quantity, 0)
-      }
-    })
-    .sortBy('total_orders')
-    .reverse()
-    .value();
+
+  var underperform = [];
+  var overperform = [];
+
+  var average = _.reduce(input, (sum, freelancer, index, collection) => {
+    return sum + freelancer.income;
+  }, 0) / input.length;
+
+  function betterThanAverage(average, freelance) {
+    return freelance.income > average;
+  }
+
+  _.forEach(input, freelance => {
+    if (betterThanAverage(average, freelance)) {
+      overperform.push(freelance);
+    } else {
+      underperform.push(freelance);
+    }
+  })
+
+  var underperformSorted = _.sortBy(underperform, 'income');
+  var overperformSorted = _.sortBy(overperform, 'income');
+
+  return {
+    average: average,
+    underperform: underperformSorted,
+    overperform: overperformSorted
+  };
 };
 
 // export the worker function as a nodejs module
