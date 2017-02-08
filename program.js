8c7d8042dc0f909a7ede70a082741ab52fb59413
Alvin BERTHELOT <alvin.berthelot@webyousoon.com>
2017-02-08 16:11:13 +0100

    (lololodash/3) : In Every Case Done

--- program.js
+++ program.js
@@ -2,7 +2,19 @@
 var _ = require("lodash");
 
 var worker = function(input) {
-  return _.sortBy(input, 'quantity').reverse();
+  return _.forEach(input, function(item) {
+    switch (true) {
+      case (item.population * 1000 > 1000):
+        item.size = 'big';
+        break;
+      case (item.population * 1000 > 500):
+        item.size = 'med';
+        break;
+      default:
+        item.size = 'small';
+    }
+    return item;
+  });
 };
 
 // export the worker function as a nodejs module
