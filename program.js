0b6c3a51cba43544f297361e35687dc0e188f09d
Alvin BERTHELOT <alvin.berthelot@webyousoon.com>
2017-02-08 16:56:19 +0100

    (lololodash/5) : Chain Mail Done

--- program.js
+++ program.js
@@ -3,21 +3,11 @@ var _ = require("lodash");
 
 var worker = function(input) {
 
-  var hot = [];
-  var warm = [];
-
-  _.forEach(input, function(values, key) {
-    if (_.every(values, value => value > 19)) {
-      hot.push(key);
-    } else if (_.some(values, value => value > 19)) {
-      warm.push(key);
-    }
-  });
-
-  return {
-    hot: hot,
-    warm: warm
-  };
+  return _.chain(input)
+    .map(item => item + 'chained')
+    .map(item => item.toUpperCase())
+    .sortBy(item => item)
+    .value();
 };
 
 // export the worker function as a nodejs module
