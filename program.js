fce4e9dae1f472ce14b8f5f321f2fdeac148bd25
Alvin BERTHELOT <alvin.berthelot@webyousoon.com>
2017-02-17 12:10:53 +0100

    Exercice 9

--- program.js
+++ program.js
@@ -1,36 +1,10 @@
 // include the Lo-Dash library
 var _ = require("lodash");
 
-var worker = function(input) {
-
-  return _.reduce(input, function(acc, freelance, index, collection){
-    var numbers = _.map(collection, function(freelance){
-      return freelance.income;
-    })
-
-    var freelancesSorted = _.sortBy(collection, function(freelance){
-      return freelance.income;
-    });
-
-    acc.average = averageIncome(numbers);
-
-    acc.underperform = _.filter(freelancesSorted, function(freelance){
-      return freelance.income <= acc.average;
-    });
-
-    acc.overperform = _.filter(freelancesSorted, function(freelance){
-      return freelance.income > acc.average;
-    });
-
-    return acc;
-  }, {});
+var worker = function(user) {
+    return _.template('Hello <%= name %> (logins: <%= logins %>)')
+                    ({name: user.name, logins: user.login.length});
 };
-
-let averageIncome = function(values){
-  return _.reduce(values, function(sum, number, index, collection){
-    return (100*sum + (100*number / collection.length))/100;
-  }, 0);
-}
 
 // export the worker function as a nodejs module
 module.exports = worker;
